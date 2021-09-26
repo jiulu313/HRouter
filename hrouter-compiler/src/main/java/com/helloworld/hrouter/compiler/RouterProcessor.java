171198f56103ccaad9601ae0d849d7a2f85bb370
1818@88.com <jiulu>
2021-09-26 11:30:21 +0800

    '优化'

--- hrouter-compiler/src/main/java/com/helloworld/hrouter/compiler/RouterProcessor.java
+++ hrouter-compiler/src/main/java/com/helloworld/hrouter/compiler/RouterProcessor.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.PrimitiveIterator;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -24,17 +25,17 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.Name;
 import javax.lang.model.element.TypeElement;
-import javax.tools.Diagnostic;
 
 @AutoService(Processor.class)
 public class RouterProcessor extends AbstractProcessor {
-    private Messager messager;
+    private static final String CORE_ROUTER_PACKAGE_NAME = "com.helloworld.hrouter.core";
+    private static final String GENERATE_CODE_PACKAGE_NAME = "com.helloworld.util";
+
     private Filer filer;
 
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
         super.init(processingEnv);
-        messager = processingEnv.getMessager();
         filer = processingEnv.getFiler();
     }
 
@@ -47,7 +48,6 @@ public class RouterProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
-        messager.printMessage(Diagnostic.Kind.NOTE,"sssssssssssssss --------------->>>>>>>>>>>>");
         Set<? extends Element> elements = roundEnvironment.getElementsAnnotatedWith(Router.class);
 
         //1. 获取到所有使用了Route注解的activity，并存入表中
@@ -55,17 +55,15 @@ public class RouterProcessor extends AbstractProcessor {
         for (Element element : elements ) {
             TypeElement typeElement = (TypeElement) element;
             Name qualifiedName = typeElement.getQualifiedName();
-            messager.printMessage(Diagnostic.Kind.NOTE,"qualifiedName=" + qualifiedName.toString());
 
             Router annotation = typeElement.getAnnotation(Router.class);
             String value = annotation.value();
-            messager.printMessage(Diagnostic.Kind.NOTE,"value=" + value);
             map.put(value,qualifiedName.toString() + ".class");
         }
 
         //写文件
         if (!map.isEmpty()) {
-            ClassName className = ClassName.get("com.helloworld.hrouter.core","IRouter");
+            ClassName className = ClassName.get(CORE_ROUTER_PACKAGE_NAME,"IRouter");
             TypeSpec.Builder typeBuilder = TypeSpec.classBuilder("ActivityUtil" + System.currentTimeMillis())
                     .addModifiers(Modifier.PUBLIC)
                     .addSuperinterface(className);
@@ -77,11 +75,11 @@ public class RouterProcessor extends AbstractProcessor {
             for (String key : map.keySet()) {
                 String value = map.get(key);
                 String s = String.format("$T.getInstance().addActivity(\"%s\",%s)",key,value);
-                addActivityBuilder.addStatement(s,ClassName.get("com.helloworld.hrouter.core","HRouter"));
+                addActivityBuilder.addStatement(s,ClassName.get(CORE_ROUTER_PACKAGE_NAME,"HRouter"));
             }
 
             TypeSpec activityUtil = typeBuilder.addMethod(addActivityBuilder.build()).build();
-            JavaFile file = JavaFile.builder("com.helloworld.util", activityUtil).build();
+            JavaFile file = JavaFile.builder(GENERATE_CODE_PACKAGE_NAME, activityUtil).build();
 
             try {
                 file.writeTo(filer);
